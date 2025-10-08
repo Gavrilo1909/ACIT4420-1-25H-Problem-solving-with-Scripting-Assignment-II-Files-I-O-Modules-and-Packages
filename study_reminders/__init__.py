0aee4a1bec3715ae62fedbfa2af2eaef02b5fe4b
Gavrilo1909 <gavrilo10c@outlook.com>
2025-10-08 16:22:59 +0200

    Add study_reminders package with all modules

---
+++ study_reminders/__init__.py
@@ -0,0 +1,6 @@
+from .students_manager import StudentsManager
+from .students import Students
+from .reminder_generator import generate_reminder
+from .reminder_sender import send_reminder
+from .logger import log_reminder
+from .scheduler import schedule_reminders
